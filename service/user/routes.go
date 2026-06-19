c6f81556c0b8739f815a5debad79b89a958923b2
Jyotindra Tavanoji <Devop@research-connectllc.com>
2026-06-19 19:11:03 +0530

    register interface done - migrations pending

--- service/user/routes.go
+++ service/user/routes.go
@@ -1,18 +1,21 @@
 package user
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/JaguarsCodehub/golang-ecom/service/auth"
 	"github.com/JaguarsCodehub/golang-ecom/types"
 	"github.com/JaguarsCodehub/golang-ecom/utils"
 	"github.com/gorilla/mux"
 )
 
 type Handler struct {
+	store types.UserStore
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{store: store}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
@@ -21,15 +24,41 @@ func (h *Handler) RegisterRoutes(router *mux.Router) {
 }
 
 func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
-
+	// get JSON payload
+	// var payload types.LoginUserPayload
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 	// get JSON payload
 	var payload types.RegisterUserPayload
-	if err := utils.ParseJSON(r.Body, payload); err != nil {
-
+	if err := utils.ParseJSON(r, payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
 	}
 	// check if the user exists
+	_, err := h.store.GetUserByEmail(payload.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("User with that email %s already exists", payload.Email))
+		return
+	}
+
+	// hash the user's password
+	hashedPassword, err := auth.HashPassword(payload.Password)
+
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, fmt.Errorf("Error in hashing password"))
+	}
+
 	// if it does not we create the new user
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		Password:  hashedPassword,
+	})
+
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, nil)
 }
